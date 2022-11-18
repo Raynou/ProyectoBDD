25fb45bcfcb2a8a770f84b90d1f8753ac6b7dfb5
Raynou <rafaelgame2012@gmail.com>
2022-11-18 11:45:12 -0600

    modify database.js

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 import { Sequelize, DataTypes } from "sequelize"
-export const sequelize = new Sequelize('proyecto_bdd', 'root', 'Supernick_145', {
+export const sequelize = new Sequelize('proyecto_bdd', 'root', 'root', {
 	host: '127.0.0.1',
 	dialect: 'mysql'
 })
